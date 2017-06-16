65849a5480c91e9212d9421289b1f81d15d2f801
Minhaz Panara <minhaz.panara@gmail.com>
2017-06-17 01:11:15 +0530

    ADDED - Explore : Static cells added.

--- WikiPro/ViewControllers/Explore/ExploreViewController.m
+++ WikiPro/ViewControllers/Explore/ExploreViewController.m
@@ -8,8 +8,11 @@
 
 #import "ExploreViewController.h"
 
-@interface ExploreViewController ()
-
+@interface ExploreViewController () <UITableViewDelegate,UITableViewDataSource>
+{
+    __weak IBOutlet UITableView *theTableView;
+    NSMutableArray *arrIdentifiers;
+}
 @end
 
 @implementation ExploreViewController
@@ -17,11 +20,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    arrIdentifiers = [NSMutableArray arrayWithObjects:
+                      @"image_cell",
+                      @"title_cell",
+                      @"subtitle_cell",
+                      @"description_title",
+                      @"description_text",
+                      nil];
+    
+    [self reloadTableData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Reload Table Data
+
+- (void)reloadTableData
+{
+    theTableView.delegate = self;
+    theTableView.dataSource = self;
+    [theTableView reloadData];
 }
 
 #pragma mark - Actions
@@ -33,5 +55,29 @@
     }];
 }
 
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *identifier = arrIdentifiers[indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    return cell.contentView.frame.size.height;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return arrIdentifiers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *identifier = arrIdentifiers[indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    return cell;
+}
 
 @end
